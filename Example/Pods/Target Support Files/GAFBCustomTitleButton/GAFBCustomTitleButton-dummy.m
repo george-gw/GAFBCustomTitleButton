08e404adcab764e69d78cad3bf898296b6cd77ec
George A <george.guidewall@gmail.com>
2016-05-13 11:31:30 +0200

    Release 0.1.0.

---
+++ Example/Pods/Target Support Files/GAFBCustomTitleButton/GAFBCustomTitleButton-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GAFBCustomTitleButton : NSObject
+@end
+@implementation PodsDummy_GAFBCustomTitleButton
+@end
---
+++ Example/Pods/Target Support Files/GAFBCustomTitleButton/GAFBCustomTitleButton-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "GAFBCustomTitleButton.h"
+
+FOUNDATION_EXPORT double GAFBCustomTitleButtonVersionNumber;
+FOUNDATION_EXPORT const unsigned char GAFBCustomTitleButtonVersionString[];
+
